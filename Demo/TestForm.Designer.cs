e2f4da7206907f90f28f88ecaca632121fa38202
Jaime Jimenez <jaime.jimenez@analog.com>
2019-08-22 18:59:34 -0400

    Initial commit of refactored codebase

---
+++ Demo/TestForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Demo
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestForm));
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            this.GotoButton = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.scintilla2 = new ScintillaNET.Scintilla();
+            this.incrementalSearcherToolStrip = new ScintillaNET_Components.IncrementalSearcher();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scintilla1.Location = new System.Drawing.Point(3, 3);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.ScrollWidth = 5001;
+            this.scintilla1.Size = new System.Drawing.Size(682, 504);
+            this.scintilla1.TabIndex = 0;
+            this.scintilla1.Text = resources.GetString("scintilla1.Text");
+            this.scintilla1.WrapMode = ScintillaNET.WrapMode.Word;
+            this.scintilla1.Enter += new System.EventHandler(this.GenericScintilla_Enter);
+            this.scintilla1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GenericScintilla_KeyDown);
+            // 
+            // GotoButton
+            // 
+            this.GotoButton.Location = new System.Drawing.Point(730, 34);
+            this.GotoButton.Name = "GotoButton";
+            this.GotoButton.Size = new System.Drawing.Size(75, 23);
+            this.GotoButton.TabIndex = 2;
+            this.GotoButton.Text = "Goto";
+            this.GotoButton.UseVisualStyleBackColor = true;
+            this.GotoButton.Click += new System.EventHandler(this.GotoButton_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 34);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(696, 580);
+            this.splitContainer1.SplitterDistance = 536;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(696, 536);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.scintilla1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(688, 510);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.scintilla2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(707, 325);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // scintilla2
+            // 
+            this.scintilla2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scintilla2.Location = new System.Drawing.Point(3, 3);
+            this.scintilla2.Name = "scintilla2";
+            this.scintilla2.ScrollWidth = 5001;
+            this.scintilla2.Size = new System.Drawing.Size(701, 319);
+            this.scintilla2.TabIndex = 1;
+            this.scintilla2.Text = resources.GetString("scintilla2.Text");
+            this.scintilla2.WrapMode = ScintillaNET.WrapMode.Word;
+            this.scintilla2.Enter += new System.EventHandler(this.GenericScintilla_Enter);
+            this.scintilla2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GenericScintilla_KeyDown);
+            // 
+            // incrementalSearcherToolStrip
+            // 
+            this.incrementalSearcherToolStrip.AutoSize = true;
+            this.incrementalSearcherToolStrip.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.incrementalSearcherToolStrip.BackColor = System.Drawing.Color.Transparent;
+            this.incrementalSearcherToolStrip.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.incrementalSearcherToolStrip.Location = new System.Drawing.Point(12, 4);
+            this.incrementalSearcherToolStrip.Manager = null;
+            this.incrementalSearcherToolStrip.Margin = new System.Windows.Forms.Padding(0);
+            this.incrementalSearcherToolStrip.Name = "incrementalSearcherToolStrip";
+            this.incrementalSearcherToolStrip.Size = new System.Drawing.Size(500, 26);
+            this.incrementalSearcherToolStrip.TabIndex = 1;
+            this.incrementalSearcherToolStrip.ToolItem = true;
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 626);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.GotoButton);
+            this.Controls.Add(this.incrementalSearcherToolStrip);
+            this.Name = "TestForm";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private ScintillaNET.Scintilla scintilla1;
+        private ScintillaNET_Components.IncrementalSearcher incrementalSearcherToolStrip;
+        private System.Windows.Forms.Button GotoButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private ScintillaNET.Scintilla scintilla2;
+    }
+}
+
---
+++ Demo/TestForm.cs
@@ -0,0 +1,100 @@
+﻿#region Using Directives
+
+using ScintillaNET;
+using ScintillaNET_Components;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+#endregion Using Directives
+
+namespace Demo
+{
+    public partial class TestForm : Form
+    {
+        private FindReplace MyFindReplace;
+        private GoTo MyGoTo;
+
+        public TestForm() {
+            InitializeComponent();
+            scintilla1.Styles[Style.Default].Font = "Consolas";
+            scintilla1.Styles[Style.Default].Size = 10;
+            scintilla2.Styles[Style.Default].Font = "Consolas";
+            scintilla2.Styles[Style.Default].Size = 10;
+            tabControl1.SelectedIndexChanged += Form_TabChanged;
+
+            MyFindReplace = new FindReplace(scintilla1);
+            MyFindReplace.Window.AutoPosition = true;
+            MyFindReplace.KeyPressed += MyFindReplace_KeyPressed;
+
+            MyGoTo = new GoTo(scintilla1);
+
+            incrementalSearcherToolStrip.Manager = MyFindReplace;
+        }
+
+        private void Form_TabChanged(object sender, EventArgs e) {
+            switch (tabControl1.SelectedIndex) {
+                case 0:
+                    scintilla1.Focus();
+                    break;
+                case 1:
+                    scintilla2.Focus();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MyFindReplace_KeyPressed(object sender, KeyEventArgs e) {
+            GenericScintilla_KeyDown(sender, e);
+        }
+
+        private void GotoButton_Click(object sender, EventArgs e) {
+            // Use the FindReplace Scintilla as this will change based on focus
+            MyGoTo.ShowDialog();
+        }
+
+        /// <summary>
+        /// Key down event for each Scintilla. Tie each Scintilla to this event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GenericScintilla_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Control && e.KeyCode == Keys.F) {
+                MyFindReplace.ShowFind();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.H) {
+                MyFindReplace.ShowReplace();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.I) {
+                MyFindReplace.ShowIncrementalSearch();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.G) {
+                MyGoTo.ShowDialog();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Escape) {
+                MyFindReplace.HideIncrementalSearch();
+                MyFindReplace.HideFindReplace();
+            }
+        }
+
+        /// <summary>
+        /// Enter event tied to each Scintilla that will share a FindReplace dialog.
+        /// Tie each Scintilla to this event.
+        /// </summary>
+        /// <param name="sender">The Scintilla receiving focus</param>
+        /// <param name="e"></param>
+        private void GenericScintilla_Enter(object sender, EventArgs e) {
+            MyFindReplace.Editor = (Scintilla)sender;
+            MyGoTo.Editor = (Scintilla)sender;
+        }
+
+        private void GenericScintilla_Enter(object sender, MouseEventArgs e) {
+
+        }
+    }
+}
